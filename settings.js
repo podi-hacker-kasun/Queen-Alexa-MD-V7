a810e0e36f4ddf37f0f36b72eaaf18348d6e0611
podi-hacker-kasun <123862013+podi-hacker-kasun@users.noreply.github.com>
2023-02-20 06:59:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94719225807'] //ur owner number
+global.owner = ['94755320928'] //ur owner number
 global.ownername = "ＱＵＥＥＮ-Ｘ ＢＯＴ" //ur owner name
 global.ytname = "CYBER JOHN" //ur yt chanel name
 global.socialm = "ＱＵＥＥＮ-Ｘ ＢＯＴ" //ur github or insta name
@@ -23,17 +23,17 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
 //ඔයාලගෙ ස්කැන් කරපුහම එන කොඩ්👇 එක දාන්න
-global.session = "YXF5RWYwZ0I=" //session Id ඔයාලගෙ ස්කැන් කරපුහම එන කොඩ් එක ඔකට දාන්න
-global.ownernomer = "94719225807" //ur number
-global.premium = ['94719225807'] //ur premium number
-global.botname = 'QUEEN -X' //ur bot name
-global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
-global.websitex = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //ur website to be displayed
+global.session = "" //session Id ඔයාලගෙ ස්කැන් කරපුහම එන කොඩ් එක ඔකට දාන්න
+global.ownernomer = "94755320928" //ur number
+global.premium = ['94755320928'] //ur premium number
+global.botname = 'Qᴜᴇᴇɴ ᴀʟᴇxᴀ ʙᴏᴛ' //ur bot name
+global.linkz = "" //your theme url which will be displayed on whatsapp
+global.websitex = "" //ur website to be displayed
 global.botscript = 'https://github.com/yureshkav/Queen-X' //script link
 global.themeemoji = "🛸" //ur theme emoji
-global.packname = "ＱＵＥＥＮ-Ｘ ＢＯＴ" //ur sticker watermark packname
-global.author = "ＱＵＥＥＮ-Ｘ ＢＯＴ\n\n\nA whatsapp bot developed by\n   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur sticker watermark author
-global.wm = "ＱＵＥＥＮ-Ｘ ＢＯＴ." //ur watermark
+global.packname = "ＱＵＥＥＮ ＡＬＥＸＡ" //ur sticker watermark packname
+global.author = "ＱＵＥＥＮ ＡＬＥＸＡ\n\n\nA whatsapp bot developed by\n   🏻Qᴜᴇᴇɴ ᴀʟᴇxᴀ ʙᴏᴛ" //ur sticker watermark author
+global.wm = "ＱＵＥＥＮ ＡＬＥＸＡ" //ur watermark
 
 // Other
 global.sessionName = 'session'
